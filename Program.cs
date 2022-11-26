eddca77f56aa5eeb799b9edeed9cde747e692e59
underdogg <und3rd099@gmail.com>
2022-11-26 15:56:21 +0300

    command handler

--- Program.cs
+++ Program.cs
@@ -2,30 +2,54 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 public class Program
 {
-  public static Task Main(string[] args) => new Program().MainAsync();
+    public static Task Main(string[] args) => new Program().MainAsync();
 
-  private DiscordSocketClient _client;
-  
-  public async Task MainAsync()
-  {
-    _client = new DiscordSocketClient();
+    private DiscordSocketClient _client;
 
-    _client.Log += Log;
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
 
-    var token = "MTA0MTgwMjUyMzUzNjc4NTUwOA.GdcfGR.oDsXubhUQu6-hC_H30g21wh68jnQaYw-W4aW1U";
+        _client.Log += Log;
+
+        var token = "MTA0MTgwMjUyMzUzNjc4NTUwOA.GdcfGR.oDsXubhUQu6-hC_H30g21wh68jnQaYw-W4aW1U";
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        CommandHandler(_client,);
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
     
-    await _client.LoginAsync(TokenType.Bot, token);
-    await _client.StartAsync();
+    static IServiceProvider CreateServices()
+    {
+        var config = new DiscordSocketConfig()
+        {
+            //...
+        };
 
-    // Block this task until the program is closed.
-    await Task.Delay(-1);
-  }
-  private Task Log(LogMessage msg)
-  {
-    Console.WriteLine(msg.ToString());
-    return Task.CompletedTask;
-  }
+        // X represents either Interaction or Command, as it functions the exact same for both types.
+        var servConfig = new XServiceConfig()
+        {
+            //...
+        }
+
+        var collection = new ServiceCollection()
+            .AddSingleton(config)
+            .AddSingleton<DiscordSocketClient>();
+            
+        return collection.BuildServiceProvider();
+    }
 }
